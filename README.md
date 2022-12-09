93e9e126681c15e59157c260e6aba8779a79c25c
Kailey <k.kotynski@gmail.com>
2022-12-08 17:17:17 -0800

    Added functions to update result state, handleClick for calc buttons

---
+++ README.md
@@ -0,0 +1,31 @@
+# React Calculator
+
+---
+
+# Project Overview
+
+This is a calculator created using React.
+
+---
+
+## Features
+
+- Update state using useState
+- Performs calculations based on user input
+
+Dashboard page:
+
+<div>
+<img src="/src/assets/calc.png">
+</div>
+
+## Technologies
+
+**Languages/Frameworks/Database:** React, CSS
+
+# <a name="about"></a>Learn More About the Developer
+
+**GitHub:** https://github.com/kotynskm
+**LinkedIn:** https://www.linkedin.com/in/kkotynski
+**Medium:** https://medium.com/@k.kotynski
+**Portfolio:** https://kotynskm.github.io/portfolio-website/
--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,90 @@ import { useState } from "react";
 import { createRoot } from "react-dom/client";
 
 const App = () => {
+  // update state to display correct result
+  const [result, setResult] = useState("");
+
+  // handle click event on each button on calculator
+  const handleClick = (e) => {
+    // use function to update state
+    setResult(result.concat(e.target.value));
+  };
+
+  // clear result
+  const clear = (e) => {
+    setResult("");
+  };
+
+  // backspace
+  const backspace = (e) => {
+    setResult(result.slice(0, result.length - 1)); // or 0, -1
+  };
+
+  // calculate the result
+  const calculate = () => {
+    try {
+      setResult(eval(result));
+    } catch {
+      setResult("Invalid Input");
+    }
+  };
+
   return (
     <div className="calculator-grid">
-      <div className="output">
-        <div className="prev-operand"></div>
-        <div className="curr-operand"></div>
-      </div>
+      <input className="output" type="text" value={result}></input>
 
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button onClick={clear} className="span-two">
+        C
+      </button>
+      <button onClick={backspace}>DEL</button>
+      <button value="/" onClick={handleClick}>
+        /
+      </button>
+      <button value="1" onClick={handleClick}>
+        1
+      </button>
+      <button value="2" onClick={handleClick}>
+        2
+      </button>
+      <button value="3" onClick={handleClick}>
+        3
+      </button>
+      <button value="*" onClick={handleClick}>
+        *
+      </button>
+      <button value="4" onClick={handleClick}>
+        4
+      </button>
+      <button value="5" onClick={handleClick}>
+        5
+      </button>
+      <button value="6" onClick={handleClick}>
+        6
+      </button>
+      <button value="+" onClick={handleClick}>
+        +
+      </button>
+      <button value="7" onClick={handleClick}>
+        7
+      </button>
+      <button value="8" onClick={handleClick}>
+        8
+      </button>
+      <button value="9" onClick={handleClick}>
+        9
+      </button>
+      <button value="-" onClick={handleClick}>
+        -
+      </button>
+      <button value="." onClick={handleClick}>
+        .
+      </button>
+      <button value="0" onClick={handleClick}>
+        0
+      </button>
+      <button onClick={calculate} className="span-two">
+        =
+      </button>
     </div>
   );
 };
