83a0eb4a320bf3657dc1026de8938111508a32b2
Kailey <k.kotynski@gmail.com>
2022-12-08 16:36:54 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="prev-operand"></div>
+        <div className="curr-operand"></div>
+      </div>
+
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+};
+
+// get div with ID root, create container, render App
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
